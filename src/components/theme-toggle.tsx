ac31953a9c2338b1a58bc3cffc3ebd06e875287e
Qwertuhh <arihantjain132407+github@gmail.com>
2025-09-19 20:36:34 +0530

    fix: theme toggle animation

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
+import { Moon, Sun, Monitor } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -11,12 +10,20 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Resolved, ThemeSelection, ThemeToggler } from "@/components/animate-ui/primitives/effects/theme-toggler";
+import { useTheme } from "next-themes";
 
 function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, resolvedTheme, setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
+    <ThemeToggler
+      theme={theme as ThemeSelection}
+      resolvedTheme={resolvedTheme as Resolved}
+      setTheme={setTheme}
+      direction="ltr" // or 'rtl', 'ttb', 'btt'
+    >
+      {/* <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
@@ -35,7 +42,39 @@ function ThemeToggle() {
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
-    </DropdownMenu>
+    </DropdownMenu> */}
+      {({ effective, toggleTheme }) => {
+        const nextTheme =
+          effective === "dark"
+            ? "light"
+            : effective === "system"
+            ? "dark"
+            : "system";
+
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => toggleTheme(nextTheme)}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => toggleTheme(nextTheme)}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => toggleTheme(nextTheme)}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      }}
+    </ThemeToggler>
   );
 }
 
